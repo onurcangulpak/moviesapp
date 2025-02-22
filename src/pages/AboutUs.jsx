6334f7cb70575c80c77f8b35395e189b3be05682
onurcangulpak <onurcangulpak@gmail.com>
2025-02-22 21:28:27 +0100

    about page done

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,26 +7,20 @@ const AboutUs = () => {
   return (
     <div className="app-container">
       <NavBar />
-     
       <div className="profile-card-out">
-      <div className="profile-card">
-        <div className="profile-img">
-          <img src={profilepic} alt="Profile" />
+        
+        <div className="info-card">
+          <div className="info-card-img">
+            <img src={profilepic} alt="Profile" />
+          </div>
+          <h4 className="h4-element">Onurcan Gulpak</h4>
+          <p className="info-element">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
+          </p>
         </div>
-        <h4 className="h4-element">Onurcan Gulpak</h4>
-        
       </div>
-
-      <div className="info-card">
-        
-        <h4 className="h4-element">Onurcan Gulpak</h4>
-        <p className="info-element">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
-        </p>
-      </div>
-    </div>
     </div>
   );
 };
